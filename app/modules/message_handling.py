4253b6790869ddac7d5e4de0f7a2a69911d6bee5
Renato Farias <piripirigoso@gmail.com>
2017-04-01 14:00:35 -0300

    messagehandling converting to dict

--- app/modules/message_handling.py
+++ app/modules/message_handling.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import app
+from ast import literal_eval
 
 class MessageHandling(object):
 
@@ -10,5 +11,9 @@ class MessageHandling(object):
         self.parse_message()
 
 
+    def str_to_dict(self):
+        return literal_eval(self.message)
+
+
     def parse_message(self):
-        app.logger.info(self.message)
+        app.logger.info(self.str_to_dict())

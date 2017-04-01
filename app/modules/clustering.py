433b7e9aae599f366342f3d048654bb7711203a0
Renato Farias <piripirigoso@gmail.com>
2017-04-01 16:29:22 -0300

    messagehandling converting to dict

---
+++ app/modules/clustering.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+class Clustering(object):
+
+    def __init__(self):
+        super(Clustering, self).__init__()
+        self.nodes = []
+
+
+    def add_node(self):
+        pass
+

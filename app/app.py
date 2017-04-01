8126aff09d1f3dd1d528c5462edd2b6f5a77d28a
Renato Farias <piripirigoso@gmail.com>
2017-04-01 12:45:07 -0300

    test thread

--- app/app.py
+++ app/app.py
@@ -34,7 +34,7 @@ def hello_world():
 def app_run(thread_name):
     logger.debug('[Thread-%d] Loading App' % thread_name)
     try:
-        app.run(debug=True, threaded=True)
+        app.run(debug=True)
     except Exception, e:
         import sys, traceback
         traceback.print_exc(file=sys.stdout)

275bbc2c84260c1aba6c4aac3a92a3cb0abad425
Renato Farias <piripirigoso@gmail.com>
2017-04-01 13:53:31 -0300

    class messagehandling

--- app/modules/message_handling.py
+++ app/modules/message_handling.py
@@ -7,7 +7,7 @@ class MessageHandling(object):
     def __init__(self, message_object):
         super(MessageHandling, self).__init__()
         self.message, self.address = message_object
-        parse_message()
+        self.parse_message()
 
 
     def parse_message(self):

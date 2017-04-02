2778c66efeece009732250ca37f2e91a3d8d4ec5
Renato Farias <piripirigoso@gmail.com>
2017-04-02 16:53:06 -0300

    clustering

--- app/routes.py
+++ app/routes.py
@@ -2,4 +2,8 @@
 
 from flask import Blueprint, current_app
 
+from views.schedule import schedule_create
+
 routes = Blueprint('routes', __name__, static_folder='../static')
+
+routes.add_url_rule('/api/schedule', view_func=schedule_create, methods=['PUT'])

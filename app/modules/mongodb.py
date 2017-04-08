462d755f047edaa5e2344b486929a7e2021fa58f
Renato Farias <piripirigoso@gmail.com>
2017-04-08 01:37:15 -0300

    simple schedule job

---
+++ app/modules/mongodb.py
@@ -0,0 +1,16 @@
+# -*- coding: UTF-8 -*-
+
+import pymongo
+
+def get_mongodb():
+    try:
+        c = pymongo.MongoClient(33.33.33.43, 27107)
+        return c['nu-tripaliare']
+    except:
+        return None
+
+def get_jobs_collection():
+    c = get_mongodb()
+    if c:
+        return c['jobs']
+    return None
--- app/views/schedule.py
+++ app/views/schedule.py
@@ -2,6 +2,7 @@
 
 from flask import request
 from modules.helpers import http_code
+from modules.mongodb import get_jobs_collection
 
 
 def schedule_create():
@@ -20,9 +21,18 @@ def schedule_create():
     put_data = request.get_json(silent=True)
 
     if not isinstance(put_data, dict):
-        return http_code(400, 'Bad Request: You should include a json in your request.')
+        return http_code(400, 'You should include a json in your request.')
 
     if not all (k in put_data.keys() for k in ('name', 'img')):
-        return http_code(400, 'Bad Request: name and img keys are required.')
+        return http_code(400, 'name and img keys are required.')
 
-    return 'ok'
+    if 'date' in put_data.keys():
+        put_date = put_data['date'].split('/')
+        if len(put_date) != 3:
+            return http_code(400, 'Date required the following format: dd/mm/yyyy.')
+
+    _d = {
+        'job_name': put_data['name'],
+        'job_img': put_data['img']
+    }
+    return get_jobs_collection.insert(_d).inserted_id

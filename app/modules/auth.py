3219b1592285c0a4a58640e40e11a7b68d596a6c
Renato Farias <piripirigoso@gmail.com>
2017-04-11 23:20:04 -0300

    api with auth

--- app/modules/auth.py
+++ app/modules/auth.py
@@ -11,7 +11,7 @@ def requires_http_auth(f):
     @wraps(f)
     def decorated(*args, **kwargs):
         auth = request.headers.get('Authorization')
-        if auth is not None and app.cache.get(auth) is not True:
+        if auth is None or app.cache.get(auth) is not True:
             return http_code(403, 'Access denied. Authorization header is missis or invalid')
         return f(*args, **kwargs)
     return decorated
